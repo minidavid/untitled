d43d8e782c67e0c9196dcb3bba8f17382ad24ecc
david <njihiadavid02@gmail.com>
2025-06-09 17:23:24 +0300

    ah sweet 2nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main()=> MaterialApp(
-  home:MyApp()
-);
+void main(){
+  runApp(
+   MaterialApp(home: MyApp())
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,8 +14,59 @@ class MyApp extends StatelessWidget {
     return Scaffold(
          appBar: AppBar(
           title: const Text('Real'),
-          flexibleSpace: Image.asset('assets/logo.png'),
+          backgroundColor: Colors.green,
          ),
+
+         body:Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            
+            children: <Widget>[
+
+              const Text(
+                'Track Your Spending Effortlessly',
+                style: TextStyle(fontSize: 40.0),
+              ),
+
+              const Text(
+                'Manage your finances easily using our intuitive and user friendly interface and set financial goals and monitor your progress',
+                style: TextStyle(fontSize: 12.0),
+              ),
+
+              ElevatedButton(
+                
+                onPressed: ()
+                {
+                  print("Button Pressed");
+                },
+                child: const Text("Get Started"),
+                
+              ),
+
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Already Have An Account?',
+                    style: TextStyle(fontSize: 12.0),                   
+                  ),
+
+                  Text(
+                    'Login',
+                    style: TextStyle(fontWeight: FontWeight.bold),                   
+                  ),
+
+
+
+                ],
+              )
+
+
+              
+
+            ],
+         ),
+        
+
+
     );
   }
 }
